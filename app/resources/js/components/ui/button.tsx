e3bb1f65116f4485b56b6610cc3b718440c6e225
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2026-05-20 16:12:36 -0300

    feat: Creating sidebar-menu to user navigation

--- app/resources/js/components/ui/button.tsx
+++ app/resources/js/components/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-white hover:bg-destructive/80 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
         link: "text-primary underline-offset-4 hover:underline",
+        sidebar: "text-[#424750] font-normal uppercase w-full rounded-lg bg-transparent justify-start hover:bg-[#2E5F9E]/80",
       },
       size: {
         default:
